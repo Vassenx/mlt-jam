00733c688f2860ae84e0415e969cb27da8650036
mngola <mikambingongola@yahoo.com>
2019-01-26 18:34:23 -0500

    Logic for tutorial level

--- Autopilot/Assets/Scripts/Controller.cs
+++ Autopilot/Assets/Scripts/Controller.cs
@@ -9,18 +9,20 @@ public class Controller : MonoBehaviour
     public Camera childCamera;
     public Transform adultBody;
     public Transform childBody;
+    public bool timeTravelEnabled;
 
     // Use this for initialization
     void Start()
     {
         adultCamera.GetComponent<Camera>().enabled = true;
         childCamera.GetComponent<Camera>().enabled = false;
+        timeTravelEnabled = true;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetButtonDown("CameraSwitch"))
+        if (Input.GetButtonDown("CameraSwitch") && timeTravelEnabled)
         {
             adultCamera.GetComponent<Camera>().enabled = !adultCamera.GetComponent<Camera>().enabled;
             childCamera.GetComponent<Camera>().enabled = !childCamera.GetComponent<Camera>().enabled;
@@ -28,5 +30,15 @@ public class Controller : MonoBehaviour
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
         adultBody.position += move * speed * Time.deltaTime;
         childBody.position += move * speed * Time.deltaTime;
+    }
+
+    void EnableTimeTravel()
+    {
+        timeTravelEnabled = true;
+    }
+
+    void DisableTimeTravel()
+    {
+        timeTravelEnabled = false;
     }
 }
