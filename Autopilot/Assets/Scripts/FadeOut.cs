be1d8f55e84a8901de7240b318f0fae76ecc61cb
Sam D <sdifeo1@gmail.com>
2019-01-26 01:01:55 -0500

    Object fade out shader and grab item basic implementation

---
+++ Autopilot/Assets/Scripts/FadeOut.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class FadeOut : MonoBehaviour
+{
+
+    //float fadeDuration = 4;
+    //float startTime = 0;
+    float transparency = 0;
+    Renderer rend;
+    bool insideBounds = false;
+
+    GameObject target;
+    float start = 0;
+
+    void Start()
+    {
+        insideBounds = false;
+        rend = GetComponent<Renderer>();
+        //startTime = Time.time;
+        target = null;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        insideBounds = true;
+        target = collision.gameObject;
+        start = target.gameObject.transform.position.x;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        insideBounds = false;
+        target = null;
+    }
+
+    void Update()
+    {
+        if (insideBounds && target != null)
+        {
+            //Debug.Log(transparency);
+            transparency = 1 - Mathf.Abs(target.transform.position.x - start) / Mathf.Abs(this.transform.position.x - start);
+            rend.material.SetFloat("_Transparency", transparency);
+        }
+        //transparency = Mathf.Lerp(0.9f, 0.0f, (Time.time - startTime) / fadeDuration);
+        // rend.material.SetFloat("_Transparency", transparency);
+    }
+}
---
+++ Autopilot/Assets/Scripts/Grab.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+    bool inBound;
+    GameObject target;
+
+    void Start()
+    {
+        inBound = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        inBound = true;
+        target = collision.gameObject;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        inBound = false;
+        target = null;
+    }
+
+    void Update()
+    {
+        //if within an item's collider and space was pressed, then grab object
+        if (inBound && target != null && Input.GetKeyDown(KeyCode.Space))
+        {
+            //hide object that was grabbed
+            this.GetComponent<Renderer>().enabled = false;
+            //Inventory.instance.addItem(this);
+        }
+    }
+}
