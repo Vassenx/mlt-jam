4c870b9881f3adc9e571c38a6daf0b0e19e8eb25
Sam D <sdifeo1@gmail.com>
2019-01-26 16:59:28 -0500

    Map scripts added and camera controller implemented

---
+++ Autopilot/Assets/Scripts/PanelHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemPanel : MonoBehaviour
+{
+    
+    GameObject descriptionPanel;
+    Image descriptionImage;
+    protected bool showPanel;
+
+    private void Awake()
+    {
+        descriptionImage = descriptionPanel.GetComponent<Image>();
+        descriptionImage.enabled = false;
+    }
+
+    void Update()
+    {
+        if (showPanel)
+        {
+
+        }
+    }
+}
